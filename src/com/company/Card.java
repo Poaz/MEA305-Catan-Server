e37ba74ec3582273a07913ff1ae33b126df69ee7
FireAndy <asvea2003@yahoo.dk>
2015-10-30 10:49:42 +0100

    Card Class

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -1,7 +1,54 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
  * Created by Dines on 27-10-2015.
  */
 public class Card {
+
+    ArrayList<String> cards = new ArrayList<String>();
+
+    public static void main(String[] args) {
+        Card card = new Card();
+        card.DevCard();
+
+    }
+
+    public void Resources() {
+        String wood;
+        String ore;
+        String wool;
+        String grain;
+        String brick;
+    }
+
+    public void DevCard() {
+        for (int i = 0; i < 13; i++) {
+            cards.add("soldier");
+        }
+        for (int j = 0; j < 1; j++) {
+            cards.add("progressM");
+            cards.add("progressF");
+            cards.add("progressB");
+        }
+        cards.add("vPMarkedplace");
+        cards.add("vPUniversity");
+        cards.add("vPParlament");
+        cards.add("vPLibrary");
+        cards.add("vPCatherdral");
+        //System.out.println(cards.get(20)); //Checks if cards are there.
+        Collections.shuffle(cards);
+        //System.out.println(cards.get(0)); //Checks if cards get shuffled.
+
+        System.out.println(cards.get(20)); //Checks if cards are there.
+    }
+
+    public String DrawDev() {
+        String a = cards.get(0);
+        cards.remove(0);
+        System.out.println(a);
+        return a;
+    }
 }
